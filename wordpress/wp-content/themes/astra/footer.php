16444ef0f179639871c4a554c999343239883bb2
DangHien-04 <dangochien2004@gmail.com>
2025-10-01 08:04:36 +0700

    update footer

--- wordpress/wp-content/themes/astra/footer.php
+++ wordpress/wp-content/themes/astra/footer.php
@@ -216,7 +216,8 @@ section .section-title {
 			
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-					<p class="h6">© <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
+				<p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
+				<p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
 				</div>
 			</div>	
 		</div>
